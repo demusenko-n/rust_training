a2320878d61e87cdc9f04b2ca10677e511cbf31e
Mykyta Demusenko <mdemusenko@outlook.com>
2025-12-22 18:25:58 +0200

    Implement solution for the task from Day 2 (#2)
    *Implement index_directory

---
+++ day2/src/main.rs
@@ -0,0 +1,35 @@
+use std::{fs, path::PathBuf};
+
+use anyhow;
+use clap::Parser;
+mod utility_files;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg()]
+    path: PathBuf,
+
+    #[arg(long, short)]
+    output_file: Option<PathBuf>,
+
+    #[arg(long, default_value_t = 1)]
+    max_depth: u32,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let result = utility_files::index_directory(&args.path, args.max_depth)?;
+
+    if !result.errors.errors.is_empty() {
+        println!("{:#?}", result.errors);
+    }
+
+    let result = format!("{:#?}", result.map);
+    if let Some(output_path) = args.output_file {
+        fs::write(&output_path, result)?;
+    } else {
+        println!("{result}");
+    };
+
+    Ok(())
+}
